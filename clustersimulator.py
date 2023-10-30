8d1ad9c36f31d019c62816b5ab2ee48eb710a8fd
Tim Greening-Jackson <tim@greening-jackson.com>
2023-10-30 18:27:13 +0000

    Tidying up

--- clustersimulator.py
+++ clustersimulator.py
@@ -38,14 +38,18 @@ class ClusterSimulator:
     def __init__(self, primary, secondary, peer, auditor):
         # Create the database connections and cursors
         self.primary = self.create_connection(primary)
+        logging.info(f"Connected to primary {self.primary.name}")
         self.secondary = self.create_connection(secondary)
+        logging.info(f"Connected to secondary {self.secondary.name}")
         self.peer = self.create_connection(peer)
+        logging.info(f"Connected to peer {self.peer.name}")
         self.auditor = self.create_connection(auditor)
+        logging.info(f"Connected to auditor {self.auditor.name}")
 
         # Get the names of the customers from the primary
 
         self.customers = self.get_customers()
-        logging.info(f'Found {len(self.customers)} customers in primary {self.priname}')
+        logging.info(f'Found {len(self.customers)} customers in primary {self.primary.name}')
 
     def __enter__(self):
         return self
@@ -56,7 +60,7 @@ class ClusterSimulator:
             logging.info(f"Closing connection to primary {self.primary.name}")
             self.primary.connection.commit()
             self.primary.connection.close()
-        if self.seccnx:
+        if self.secondary.connection:
             logging.info(f"Closing connection to secondary {self.secondary.name}")
             self.secondary.connection.commit()
             self.secondary.connection.close()
@@ -89,52 +93,21 @@ class ClusterSimulator:
 
         sql = "SELECT id, name FROM customers"
         self.primary.cursor.execute(sql)
-        customers = {row[0]:row[1] for row in self.pricur.fetchall()}
+        customers = {row[0]:row[1] for row in self.primary.cursor.fetchall()}
         if len(customers) == 0:
             logging.critical("No customers found in primary database")
             sys.exit(1)
         return customers
 
-    def insert_transactions(self, name, cursor, number=1):
-        logging.info(f"Inserting {number} transactions into {name}")
+    def insert_transactions(self, target, number=1):
+        logging.info(f"Inserting {number} transactions into {target.name}")
         for i in range(number):
             customer = random.choice(list(self.customers.keys()))
             amount = random.randint(-10000,10000)/100
             uu = str(uuid.uuid4())
             sql = 'INSERT INTO transactions (node, uuid, customer_id, amount) VALUES (?, ?, ?, ?)'
-            cursor.execute(sql, (name, uu, customer, amount))
+            target.cursor.execute(sql, (target.name, uu, customer, amount))
             sql = 'INSERT INTO audit (node, uuid, customer_id, amount) VALUES (?, ?, ?, ?)'
-            self.audcur.execute(sql, (name, uu, customer, amount))
+            self.auditor.cursor.execute(sql, (target.name, uu, customer, amount))
             pass
 
-if __name__ == '__main__':
-    description = "Sets up an initial corpus of data for the simulation."
-    epilog = "Copyright \N{COPYRIGHT SIGN} 2023 Snowy Consultants Limited"
-    ap = argparse.ArgumentParser(description=description, epilog=epilog)
-    ap.add_argument('-c', '--config', help='Path to configuration file', required=True)
-    ap.add_argument('-d', '--debug', help='Enable debug logging', action='store_true')
-    args = ap.parse_args()
-
-    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
-                        format='%(asctime)s %(levelname)-8s %(message)s')
-
-    logging.info(f"Reading configuration from {args.config}")
-    try:
-        with open(args.config, 'r') as f:
-            config = yaml.safe_load(f)
-    except FileNotFoundError:
-        logging.critical(f"Configuration file {args.config} not found")
-        print(f"Configuration file {args.config} not found")
-        sys.exit(1)
-
-    try:
-        with ClusterSimulator(primary=config['primary'],
-                     secondary=config['secondary'],
-                     peer=config['peer'],
-                     auditor=config['auditor']) as sim:
-            pass
-            sim.insert_transactions(sim.priname, sim.pricur, 1000)
-    except (mariadb.OperationalError,mariadb.ProgrammingError, mariadb.NotSupportedError) as e:
-        logging.critical(f"Unable to connect to database: {e}")
-        print(f"Unable to connect to database: {e}")
-        sys.exit(1)
